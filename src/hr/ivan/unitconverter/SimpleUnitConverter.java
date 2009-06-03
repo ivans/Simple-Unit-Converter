b929f3f6bb523def3b3aedeec87f507091b9e9f0
Ivan Senji <ivan.senji@gmail.com>
2009-06-03 22:31:12 +0200

    Dodao layout za listu jedinica, i prikazao listu jedinica iz stringova...

--- src/hr/ivan/unitconverter/SimpleUnitConverter.java
+++ src/hr/ivan/unitconverter/SimpleUnitConverter.java
@@ -1,13 +1,18 @@
 package hr.ivan.unitconverter;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class SimpleUnitConverter extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class SimpleUnitConverter extends ListActivity {
+
+	String[] items = { "Lenght", "Time", "Mass" };
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		setListAdapter(new ArrayAdapter<String>(this,
+				R.layout.unit_list_row_layout, R.id.unit_list_label, items));
+	}
 }
