296aad45883e7df2a3004bc3bb051f61c3433780
unknown <ivans@.in2.hr>
2009-06-15 14:55:06 +0200

    Pokusaj igranja s kamerom

--- src/hr/ivan/unitconverter/UnitActivity.java
+++ src/hr/ivan/unitconverter/UnitActivity.java
@@ -1,10 +1,21 @@
 package hr.ivan.unitconverter;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
 
 public class UnitActivity extends Activity {
+
+	private TextView mTextDebug;
+	private SurfaceView mSurfaceCamera;
+	private Camera mCamera;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -13,6 +24,25 @@ public class UnitActivity extends Activity {
 				"hr.ivan.unitconverter.unitname");
 		Log.d("UnitActivity", "showing activity for unit: " + unitName);
 		setContentView(R.layout.main);
+
+		mTextDebug = (TextView) findViewById(R.id.text_debug);
+		mSurfaceCamera = (SurfaceView) findViewById(R.id.surface_camera);
+
+		mCamera = Camera.open();
+		try {
+			mCamera.setPreviewDisplay(mSurfaceCamera.getHolder());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Log.d("UnitActivity", "Camera: " + mCamera);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (mCamera != null) {
+			mCamera.release();
+		}
 	}
 
 }
